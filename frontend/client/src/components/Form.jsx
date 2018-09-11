d1ebcce73fa91645b6cc2535bdaef17579eb6989
Jacob  Metzinger <jacobmetzinger@Jacobs-MacBook-Pro.local>
2018-09-10 23:23:40 -0700

    Set up frontend skeleton

---
+++ frontend/client/src/components/Form.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+  render () {
+    return (<div>Hey</div>)
+  }
+}
---
+++ frontend/client/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import Form from './components/From.jsx';
+
+
+render(<Form />, document.getElementById('app'));
